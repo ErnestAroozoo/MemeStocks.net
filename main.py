2c4ce88c6313e7c2ff43e69419666a734f481724
Ernest Aroozoo <ernestaroozoo@gmail.com>
2022-06-01 06:22:01 -0400

    Added loading bar Added different subreddits Fixed exception for ETFs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import reddit_scraper
 import yahoo_finance
+import time
 
 # Page config
 st.set_page_config(
@@ -30,31 +32,47 @@ def stock_info():
     st.metric(label="Stock Name", value=stock_name)
     progress_bar.progress(20)
 
-    stock_sector = stock_data['sector']
-    st.metric(label="Sector", value=stock_sector)
+    try:
+        stock_sector = stock_data['sector']
+        st.metric(label="Sector", value=stock_sector)
+    except:
+        pass
+
     progress_bar.progress(40)
 
-    stock_country = stock_data['country']
-    st.metric(label="Country", value=stock_country)
+    try:
+        stock_country = stock_data['country']
+        st.metric(label="Country", value=stock_country)
+    except:
+        pass
+
     progress_bar.progress(60)
 
-    stock_price = stock_data['regularMarketPrice']
+    stock_currency = stock_data['currency']
+    stock_price = "$" + str(stock_data['regularMarketPrice']) + " " + stock_currency
     st.metric(label="Stock Price", value=stock_price)
     progress_bar.progress(80)
 
-    stock_mentions = reddit_scraper.stock_mentions(search_input)
+    stock_mentions = reddit_scraper.stock_mentions(search_input, subreddit_input)
     st.metric(label="Today's Number of Mentions", value=stock_mentions)
 
     progress_bar.progress(100)
+
+    time.sleep(3)
+
+    progress_bar.empty()
 
 
 # Search input
 st.header('')
 search_input = st.text_input('Stock Ticker', '', max_chars=10, key=str, placeholder='Type a stock ticker (e.g. "AAPL")')
+subreddit_input = st.selectbox('Subreddit', ('WallStreetBets', 'Stocks', 'Investing'))
 
 # Display information if stock exists
-if search_input != '' and yahoo_finance.stock_exists(search_input) is True:
-    stock_info()
-# Return error message if stock does not exist
-elif search_input != '' and yahoo_finance.stock_exists(search_input) is False:
-    st.error('Error: Invalid stock symbol. Try a valid stock symbol such as "AAPL".')
+with st.spinner("Please wait... Retrieving information from" + " " + "r/" + subreddit_input):
+    if search_input != '' and yahoo_finance.stock_exists(search_input) is True:
+        stock_info()
+
+    # Return error message if stock does not exist
+    elif search_input != '' and yahoo_finance.stock_exists(search_input) is False:
+        st.error('Error: Invalid stock symbol. Try a valid stock symbol such as "AAPL".')
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -3,7 +3,7 @@ import praw
 
 
 # Scraping process //
-def stock_mentions(stock_ticker):
+def stock_mentions(stock_ticker, subreddit):
     # Declare Reddit Information
     reddit = praw.Reddit(client_id='EXQz_JfwBsIDzxPleRVxUg', client_secret='wSRhPRaYmr-uN_htZhnfPwSVJPkTnQ',
                          redirect_uri='https://memestocks.net', user_agent='memestocks_net')
@@ -14,7 +14,7 @@ def stock_mentions(stock_ticker):
     # Whitelisted words to scrape
     whitelist = [stock_ticker.upper(), "$" + stock_ticker.upper()]
     # Search every new post for the whitelisted words
-    for submission in reddit.subreddit("wallstreetbets").new(limit=200):
+    for submission in reddit.subreddit(subreddit).new(limit=100):
         submission_title = submission.title.upper()
         submission_title_keywords = submission_title.split()
         submission_date = datetime.datetime.fromtimestamp(submission.created).strftime("%x")
