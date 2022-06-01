425bed7633cebb5e668e933e8ff37b0b5a0c6de5
Ernest Aroozoo <ernestaroozoo@gmail.com>
2022-06-01 07:28:21 -0400

    Removed default Streamlit styling and changed it

--- main.py
+++ main.py
@@ -17,6 +17,61 @@ st.set_page_config(
     }
 )
 
+# Remove Streamlit hamburger menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: visible;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Change Streamlit footer
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #212121;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Made by Ernest Aroozoo</p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+# Remove Streamlit top margin
+hide_streamlit_style = """
+<style>
+    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Remove Streamlit top decoration bar
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
 # Title of the web app
 st.title("MemeStocks.net")
 
@@ -57,9 +112,7 @@ def stock_info():
     st.metric(label="Today's Number of Mentions", value=stock_mentions)
 
     progress_bar.progress(100)
-
-    time.sleep(3)
-
+    time.sleep(1)
     progress_bar.empty()
 
 
@@ -69,21 +122,10 @@ search_input = st.text_input('Stock Ticker', '', max_chars=10, key=str, placehol
 subreddit_input = st.selectbox('Subreddit', ('WallStreetBets', 'Stocks', 'Investing'))
 
 # Display information if stock exists
-with st.spinner("Please wait... Retrieving information from" + " " + "r/" + subreddit_input):
+with st.spinner("Please wait... Retrieving data from" + " " + "r/" + subreddit_input):
     if search_input != '' and yahoo_finance.stock_exists(search_input) is True:
         stock_info()
 
     # Return error message if stock does not exist
     elif search_input != '' and yahoo_finance.stock_exists(search_input) is False:
         st.error('Error: Invalid stock symbol. Try a valid stock symbol such as "AAPL".')
-
-# Remove Streamlit watermark
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-
