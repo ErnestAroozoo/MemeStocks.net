ab83f3fd3cf2351f236679fdb772184f9c421609
Ernest Aroozoo <ernestaroozoo@gmail.com>
2022-06-01 20:42:31 -0400

    Added TradingView chart

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Change Streamlit footer
-footer="""<style>
+footer = """<style>
 a:link , a:visited{
 color: blue;
 background-color: transparent;
@@ -54,7 +54,7 @@ text-align: center;
 <p>Made by Ernest Aroozoo</p>
 </div>
 """
-st.markdown(footer,unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)
 
 # Remove Streamlit top margin
 hide_streamlit_style = """
@@ -85,46 +85,83 @@ st.title("MemeStocks.net")
 
 # Function to display stock information
 def stock_info():
+    col1, col2 = st.columns(2)
+
     progress_bar = st.progress(0)
     progress_bar.progress(0)
 
     stock_data = yahoo_finance.get_info(search_input)  # Retrieves dictionary object with info
 
-    stock_name = stock_data['shortName']
-    st.metric(label="Stock Name", value=stock_name)
-    progress_bar.progress(20)
+    with col1:
+        stock_name = stock_data['shortName']
+        st.metric(label="Stock Name", value=stock_name)
+        progress_bar.progress(20)
 
-    try:
-        stock_sector = stock_data['sector']
-        st.metric(label="Sector", value=stock_sector)
-    except:
-        pass
+        try:
+            stock_sector = stock_data['sector']
+            st.metric(label="Sector", value=stock_sector)
+        except:
+            pass
 
-    progress_bar.progress(40)
+        progress_bar.progress(40)
 
-    try:
-        stock_country = stock_data['country']
-        st.metric(label="Country", value=stock_country)
-    except:
-        pass
+        try:
+            stock_country = stock_data['country']
+            st.metric(label="Country", value=stock_country)
+        except:
+            pass
 
-    progress_bar.progress(60)
+        progress_bar.progress(60)
 
-    stock_currency = stock_data['currency']
-    stock_price = "$" + str(stock_data['regularMarketPrice']) + " " + stock_currency
-    st.metric(label="Stock Price", value=stock_price)
-    progress_bar.progress(80)
+    with col2:
+        stock_currency = stock_data['currency']
+        stock_price = "$" + str(stock_data['regularMarketPrice'])
+        st.metric(label="Stock Price", value=stock_price)
+        progress_bar.progress(80)
 
-    stock_mentions = reddit_scraper.stock_mentions(search_input, subreddit_input)
-    st.metric(label="Today's Number of Mentions", value=stock_mentions)
+        stock_mentions = reddit_scraper.stock_mentions(search_input, subreddit_input)
+        st.metric(label="Number of Posts (Today)", value=stock_mentions)
 
-    progress_bar.progress(100)
-    time.sleep(1)
-    progress_bar.empty()
+        progress_bar.progress(100)
+        time.sleep(1)
+        progress_bar.empty()
+
+
+# Function to display TradingView chart
+def stock_chart():
+    with st.container():
+        components.html(
+            """
+    <!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container">
+      <div id="tradingview_c45b8"></div>
+      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+      <script type="text/javascript">
+      new TradingView.widget(
+      {
+      "width": 1000,
+      "height": 500,
+      "symbol":""" "\"" + str(search_input) + "\"" + """,
+          "interval": "D",
+          "timezone": "exchange",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "toolbar_bg": "#f1f3f6",
+          "enable_publishing": false,
+          "hide_top_toolbar": true,
+          "save_image": false,
+          "container_id": "tradingview_c45b8"
+        }
+          );
+          </script>
+        </div>
+        <!-- TradingView Widget END -->
+                """, height=500, scrolling=False
+        )
 
 
 # Search input
-st.header('')
 search_input = st.text_input('Stock Ticker', '', max_chars=10, key=str, placeholder='Type a stock ticker (e.g. "AAPL")')
 subreddit_input = st.selectbox('Subreddit', ('WallStreetBets', 'Stocks', 'Investing'))
 
@@ -132,6 +169,7 @@ subreddit_input = st.selectbox('Subreddit', ('WallStreetBets', 'Stocks', 'Invest
 with st.spinner("Please wait... Retrieving data from" + " " + "r/" + subreddit_input):
     if search_input != '' and yahoo_finance.stock_exists(search_input) is True:
         stock_info()
+        stock_chart()
 
     # Return error message if stock does not exist
     elif search_input != '' and yahoo_finance.stock_exists(search_input) is False:
