f6269cac1e265bffb213d71e433f40f185c084dc
Ernest Aroozoo <ernestaroozoo@gmail.com>
2022-08-21 01:35:19 -0400

    CHANGES: - Changed Streamlit frontend layout - Fixed user input if there are empty spaces
    TODO:
    - Fix reddit_scraper.py so that data can be aggregated and stored in PostgreSQL

--- main.py
+++ main.py
@@ -101,31 +101,38 @@ st.markdown(hide_anchor_style, unsafe_allow_html=True)
 
 # FUNCTION: Display stock information
 def stock_info():
-    st.subheader('Information')
-
     # Create columns for section 1
-    whitelist_index = -1
-    for k in whitelist:
-        whitelist_index = whitelist_index + 1
-        if search_input.upper() == k:
-            break
-
     col1, col2 = st.columns(2)
 
     with col1:
-
-        st.metric(label="Stock Symbol", value="$"+search_input.upper())
+        st.subheader('Information')
+        st.metric(label="Stock Symbol", value="$" + search_input.upper())
+        st.metric(label="Name", value=stock_name_list[whitelist.index(search_input)])
 
     with col2:
-        st.metric(label="Name", value=stock_name_list[whitelist_index])
+        st.subheader('Mentions Data')
+        database_name = search_input.lower()
+        # Create Pandas object using data from PostgreSQL
+        df = pd.read_sql_table(database_name, connection)
+        # Filter columns based on user selection (e.g. subreddit)
+        df_filter = df[["date", subreddit_input.lower() + "_posts", subreddit_input.lower() + "_posts_data",
+                        subreddit_input.lower() + "_comments", subreddit_input.lower() + "_comments_data"]]
+        # Create new table with renamed column titles
+        df2 = df_filter.set_axis(['Date', '# of Posts', 'Posts', '# of Comments', 'Comments'], axis=1, inplace=False)
+        # Sort data by date
+        df2 = df2.sort_values(by='Date', ascending=False)
+        # Change date format to YY-MM-DD
+        df2["Date"] = pd.to_datetime(df2["Date"]).dt.date
+        # Display Pandas table
+        st.dataframe(data=df2, height=160, width=1000)
 
     st.markdown("""---""")
 
     # Create columns for section 2
     with st.container():
-        col5, col6 = st.columns([4, 3])
+        col3, col4 = st.columns(2)
 
-        with col5:
+        with col3:
             st.subheader('Price Chart')
             components.html(
                 """
@@ -136,7 +143,7 @@ def stock_info():
           <script type="text/javascript">
           new TradingView.widget(
           {
-          "width": 800,
+          "width": 700,
           "height": 500,
           "symbol":""" "\"" + str(search_input.upper()) + "\"" + """,
                "interval": "D",
@@ -147,6 +154,7 @@ def stock_info():
                "toolbar_bg": "#f1f3f6",
                "enable_publishing": false,
                "hide_top_toolbar": true,
+               "hide_legend": true,
                "save_image": false,
                "container_id": "tradingview_c45b8"
              }
@@ -154,22 +162,13 @@ def stock_info():
                </script>
              </div>
              <!-- TradingView Widget END -->
-                     """, width=800, height=500, scrolling=False
+                     """, width=700, height=500, scrolling=False
             )
 
-        with col6:
-            st.subheader('Mentions Data')
-            database_name = search_input.lower()
-            # Create Pandas object
-            df = pd.read_sql_table(database_name, connection)
+        with col4:
+            st.subheader('Sentiment Chart')
+            st.caption("Data Unavailable")
 
-            # df_filter = df['Stock Symbol'] == search_input.upper()
-            # df_filtered = df[df_filter]
-            #
-            # df_filtered.set_index('Date', inplace=True)
-            # st.dataframe(data=df_filtered, width=1000)
-
-            st.dataframe(data=df, width=1000)
     con.close()
 
 
@@ -177,8 +176,8 @@ def stock_info():
 st.title("📈 MemeStocks.net")
 
 # Search input
-search_input = st.text_input('Stock Symbol', '', max_chars=5, key=str,
-                             placeholder="Type a stock symbol and then press enter (e.g. GME)")
+search_input = (st.text_input('Stock Symbol', '', max_chars=5, key=str,
+                              placeholder="Type a stock symbol and then press enter (e.g. GME)")).strip().upper()
 subreddit_input = st.selectbox('Subreddit', ['WallStreetBets'])
 
 # Case 1: Display information if stock exists
@@ -187,7 +186,7 @@ if search_input.upper() != '' and search_input.upper() in whitelist:
 
 # Case 2: Return error message if stock does not exist
 elif search_input.upper() != '' and search_input.upper() not in whitelist:
-    st.error("Error: Invalid stock symbol. Please type a valid stock symbol such as AAPL.")
+    st.error("Error: Invalid stock symbol. Please type a valid stock symbol (e.g. AAPL).")
 
 # CUSTOMIZATION: Change Streamlit footer again to prevent clipping
 add_footer_style2 = """<style>
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,9 +1,9 @@
-import datetime
 from datetime import datetime
-from datetime import timedelta
+import datetime as dt
 import praw
 import psycopg2
 import csv
+from psaw import PushshiftAPI
 from psycopg2 import sql
 
 # Heroku PostgreSQL Database
@@ -24,50 +24,80 @@ with open('stock_tickers.csv', newline='') as f:
     reddit = praw.Reddit(client_id='EXQz_JfwBsIDzxPleRVxUg', client_secret='wSRhPRaYmr-uN_htZhnfPwSVJPkTnQ',
                          redirect_uri='https://memestocks.net', user_agent='memestocks_net')
 
-# Initialize today's date in YYYY-MM-DD format
-today = datetime.today().utcnow().strftime('%Y-%m-%d')  # Retrieve today's date in UTC
+
+# # FUNCTION: Store Reddit posts as a list locally for more optimal scraping
+# def scrape_posts(subreddit_name):
+#     # Initialize variables
+#     posts_list = []  # List used to store the title of all posts within the time range
+#     for posts in reddit.subreddit(subreddit_name).new(limit=1000000):
+#         post_date = datetime.utcfromtimestamp(posts.created).strftime('%Y-%m-%d')  # Retrieve post's date in UTC
+#         # Case 1: If the post date is today then add it to the list
+#         if post_date == today:
+#             posts_list.append(posts.title)
+#             print(post_date)
+#         # Case 2: If post date is not today meaning we can end loop early
+#         else:
+#             break
+#     return posts_list
 
 
-# FUNCTION: Store Reddit posts as a list locally for more optimal scraping
-def scrape_posts(subreddit_name):
-    # Initialize variables
-    posts_list = []  # List used to store the title of all posts within the time range
-    for posts in reddit.subreddit(subreddit_name).new(limit=100000):
-        post_date = datetime.utcfromtimestamp(posts.created).strftime('%Y-%m-%d')  # Retrieve post's date in UTC
-        # Case 1: If the post date is today then add it to the list
-        if post_date == today:
-            posts_list.append(posts.title)
-            print(post_date)
-        # Case 2: If post date is not today meaning we can end loop early
-        else:
-            break
-    return posts_list
+# FUNCTION: Scrape historical data using PushShift
+def scrape_historical_posts(subreddit_name):
+    # Initialize PushShift API
+    api = PushshiftAPI()
+    # Initialize the time to search for
+    start_time = int(dt.datetime(2022, 1, 1).timestamp())
+    print("After (Unix): " + str(start_time))
+    print("After (YYYY-MM-DD): " + datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d"))
+    # Search through all the stock tickers
+    for ticker in data:
+        stock_symbol = ticker[0]
+        comments = list(api.search_comments(after=start_time,
+                                            q=str(stock_symbol),
+                                            subreddit=subreddit_name,
+                                            filter=['url', 'author', 'title', 'subreddit', 'body']))
+        submissions = list(api.search_comments(after=start_time,
+                                               q=str(stock_symbol),
+                                               subreddit=subreddit_name,
+                                               filter=['url', 'author', 'title', 'subreddit']))
+
+        for submission in submissions:
+            submission_title = submission.title
+            submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime("%Y-%m-%d")
+            cur.execute("""
+            INSERT INTO %s (date, wallstreetbets_posts, wallstreetbets_posts_data)
+            VALUES (%s, %s, %s) 
+            """, (stock_symbol, submission_date))
+
+        for comment in comments:
+            comment_body = comment.body
+            comment_date = datetime.utcfromtimestamp(submission.created_utc).strftime("%Y-%m-%d")
 
 
-# FUNCTION: Parse the list of titles for specified keywords and commit to database
-def parse_posts(posts_list):
-    for tickers in data:
-        # Initialize variables
-        ticker = tickers[0]
-        whitelist_keyword1 = ticker.upper()
-        whitelist_keyword2 = '$' + ticker.upper()
-        mentions_posts = 0
-        content = []
-        for titles in posts_list:
-            post_title_keywords = titles.upper().split()
-            # Case 1: Post date is today and keywords are in the title
-            if whitelist_keyword1 in post_title_keywords or whitelist_keyword2 in post_title_keywords:
-                mentions_posts = mentions_posts + 1  # Count number of mentions from posts
-                content.append(titles)  # Append post title into a list
-
-        print("Symbol: " + ticker + " | Mentions: " + str(mentions_posts))
-        print(content)
-        # Insert data into PostgreSQL
-        cur.execute("""
-        INSERT INTO mentions_posts (date, stock_symbol, subreddit, mentions_posts, content)
-        VALUES (%s, %s, %s, %s, %s)
-        """, (today, ticker, 'wallstreetbets', mentions_posts, content))
-    con.commit()
+# # FUNCTION: Parse the list of titles for specified keywords and commit to database
+# def parse_posts(posts_list):
+#     for tickers in data:
+#         # Initialize variables
+#         ticker = tickers[0]
+#         whitelist_keyword1 = ticker.upper()
+#         whitelist_keyword2 = '$' + ticker.upper()
+#         mentions_posts = 0
+#         content = []
+#         for titles in posts_list:
+#             post_title_keywords = titles.upper().split()
+#             # Case 1: Post date is today and keywords are in the title
+#             if whitelist_keyword1 in post_title_keywords or whitelist_keyword2 in post_title_keywords:
+#                 mentions_posts = mentions_posts + 1  # Count number of mentions from posts
+#                 content.append(titles)  # Append post title into a list
+#
+#         print("Symbol: " + ticker + " | Mentions: " + str(mentions_posts))
+#         print(content)
+#         # Insert data into PostgreSQL
+#         cur.execute("""
+#         INSERT INTO mentions_posts (date, stock_symbol, subreddit, mentions_posts, content)
+#         VALUES (%s, %s, %s, %s, %s)
+#         """, (today, ticker, 'wallstreetbets', mentions_posts, content))
+#     con.commit()
 
 
 # # FUNCTION: Only need to run once to initialize databases for all stocks
@@ -93,8 +123,9 @@ def parse_posts(posts_list):
 
 
 if __name__ == "__main__":
-    wallstreetbets_posts_list = scrape_posts('wallstreetbets')  # Retrieve a list of posts from r/wallstreetbets
-    parse_posts(wallstreetbets_posts_list)  # Parse title from r/wallstreetbets for keywords
-
-    cur.close()
-    con.close()
+    # wallstreetbets_posts_list = scrape_posts('wallstreetbets')  # Retrieve a list of posts from r/wallstreetbets
+    # parse_posts(wallstreetbets_posts_list)  # Parse title from r/wallstreetbets for keywords
+    # cur.close()
+    # con.close()
+    # scrape_historical_posts("wallstreetbets")
+    print("test")
