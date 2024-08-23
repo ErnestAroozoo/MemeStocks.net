69361cfb4a9521cddc3fdc6ed93bcfca76175b18
Ernest Aroozoo <45665527+ErnestAroozoo@users.noreply.github.com>
2024-08-23 22:29:34 +0800

    Refactored code

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # MemeStocks.net
 
 ### What is MemeStocks.net?
-MemeStocks.net is a web application that tracks the historical popularity of specific stocks by monitoring the number of mentions on Reddit. Users can search for a stock symbol and view information such as the stock's name, price, and historical popularity data. The data is gathered through web scraping and stored in a PostgreSQL database hosted on Heroku. The application generates multiple time-series graphs to display the historical popularity of the stock, allowing users to see trends in the stock's popularity over time and make more informed decisions about their investments.
+MemeStocks.net is a web application that tracks the historical popularity of specific stocks by monitoring Reddit mentions. Users can search for a stock symbol to access detailed information, including the stock's name, price, and historical popularity data. The data is collected using the Pushshift API and stored in a PostgreSQL database. The application generates time-series graphs to visually display the stock's popularity trends over time, helping users make more informed investment decisions.
 
 ![](https://github.com/ErnestAroozoo/MemeStocks.net/blob/main/tutorial.gif)
-
-### How Does It Work?
-The web application uses web scraping techniques to gather historical data on stock mentions from Reddit, utilizing the PMAW library to access the Pushshift.io API. The scraping process is automated on a daily basis using Heroku Scheduler, making sure that the data is always up-to-date. The collected data is then stored in a PostgreSQL database hosted on Heroku for easy access and analysis.
-
-When a user enters a stock symbol, the application employs the Pandas library to retrieve and filter relevant data from the database. This data is then utilized to generate multiple time-series graphs that display the historical popularity of the stock. These graphs allow users to identify trends in the stock's popularity over time. The application also employs the Streamlit library to create an interactive user interface, which enables users to easily search for and view stock information.
-
-The psycopg2 and SQLAlchemy libraries are utilized to connect to the PostgreSQL database, allowing for easy querying of the data. The Pandas library is employed to filter and process the data before it is displayed in the time-series graph.
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from datetime import datetime
 from pytz import timezone
+import duckdb
 
 # CUSTOMIZATION: Page config
 st.set_page_config(
@@ -58,31 +59,7 @@ hide_streamlit_style = """
                 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-# CUSTOMIZATION: Add custom footer
-add_footer_style = """<style>
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #212121;
-color: #f1f1f1;
-text-align: center;
-padding: 2px;
-font-size: 12px;
-}
-a {
-color: #f1f1f1;
-text-decoration: none;
-}
-</style>
-<div class="footer">
-<p>Made by <a href='https://github.com/ErnestAroozoo' target='_blank'>Ernest Aroozoo</a> | <a href='https://github.com/ErnestAroozoo/MemeStocks.net' target='_blank'>View on GitHub</a></p>
-</div>
-"""
-st.markdown(add_footer_style, unsafe_allow_html=True)
-
-# Heroku PostgreSQL Database
+# CONNECTION: PostgreSQL Database
 load_dotenv()
 con = psycopg2.connect(
     host=os.getenv("DB_HOST"),
@@ -95,7 +72,7 @@ cur = con.cursor()
 db_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 connection = create_engine(db_string, pool_size=20, max_overflow=0).connect()
 
-# Stock Tickers CSV Database
+# CONNECTION: Stock Tickers CSV Dataset
 with open('stock_tickers.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
@@ -113,7 +90,7 @@ def stock_info():
         icon="⚠️")
     # Create columns for section 1
 
-    st.subheader('Information')
+    st.subheader('Information', anchor=False)
     col1, col2 = st.columns(2)
 
     with col1:
@@ -153,39 +130,40 @@ def stock_info():
 
     st.markdown("""---""")
 
-    st.subheader('Mentions Data')
-    database_name = search_input.lower()
+    st.subheader('Mentions Data', anchor=False)
+    
     # Create Pandas object using data from PostgreSQL
+    database_name = search_input.lower()
     df = pd.read_sql_table(database_name, connection)
-    # Filter columns based on user selection (e.g. subreddit)
-    df_filter = df[["date", subreddit_input.lower() + "_posts", subreddit_input.lower() + "_posts_data",
-                    subreddit_input.lower() + "_comments", subreddit_input.lower() + "_comments_data"]]
-    # Create new table with renamed column titles
-    df2 = df_filter.set_axis(['Date', '# of Posts', 'Posts', '# of Comments', 'Comments'], axis=1, inplace=False)
-    # Sort data by date
-    df2 = df2.sort_values(by='Date', ascending=False)
+    df = duckdb.query(f"""
+                      SELECT date AS "Date", wallstreetbets_posts AS "Number of Posts", wallstreetbets_posts_data AS "Posts", 
+                      wallstreetbets_comments AS "Number of Comments", wallstreetbets_comments_data AS "Comments"
+                      FROM df
+                      ORDER BY "Date" DESC
+                      """).to_df()
     # Change date format to YY-MM-DD
-    df2["Date"] = pd.to_datetime(df2["Date"]).dt.date
+    df["Date"] = pd.to_datetime(df["Date"]).dt.date
+  
     # Display Pandas table
-    st.dataframe(data=df2, height=140, width=3000, hide_index=True)
+    st.dataframe(data=df, height=140, width=3000, hide_index=True)
 
     # Create columns for section 2
     with st.container():
         col3, col4, col5 = st.columns(3)
         with col3:
-            st.line_chart(data=df2, x='Date', y='# of Posts', width=1000, height=250, use_container_width=True)
+            st.line_chart(data=df, x='Date', y='Number of Posts', width=1000, height=250, use_container_width=True)
         with col4:
-            st.line_chart(data=df2, x='Date', y='# of Comments', width=1000, height=250, use_container_width=True)
+            st.line_chart(data=df, x='Date', y='Number of Comments', width=1000, height=250, use_container_width=True)
         with col5:
-            df2["# of Posts & Comments"] = df2["# of Posts"] + df2["# of Comments"]
-            st.line_chart(data=df2, x='Date', y='# of Posts & Comments', width=1000, height=250,
+            df["Number of Posts & Comments"] = df["Number of Posts"] + df["Number of Comments"]
+            st.line_chart(data=df, x='Date', y='Number of Posts & Comments', width=1000, height=250,
                           use_container_width=True)
 
     con.close()
 
 
 # Title of the web app
-st.title("📈 MemeStocks.net")
+st.title("📈 MemeStocks.net", anchor=False)
 
 # Search input
 search_input = (st.text_input('Stock Symbol', '', max_chars=5, key=str,
@@ -204,22 +182,12 @@ with intro.container():
     st.markdown("""
     ------------
     ### What is MemeStocks.net?
-    MemeStocks.net is a web application that tracks the historical popularity of specific stocks by monitoring the number of mentions on Reddit. Users can search for a stock symbol and view information such as the stock's name, price, and historical popularity data. The data is gathered through web scraping and stored in a PostgreSQL database hosted on Heroku. The application generates multiple time-series graphs to display the historical popularity of the stock, allowing users to see trends in the stock's popularity over time and make more informed decisions about their investments.
+    MemeStocks.net is a web application that tracks the historical popularity of specific stocks by monitoring Reddit mentions. Users can search for a stock symbol to access detailed information, including the stock's name, price, and historical popularity data. The data is collected using the Pushshift API and stored in a PostgreSQL database. The application generates time-series graphs to visually display the stock's popularity trends over time, helping users make more informed investment decisions.
     """)
     # Add gif
     col1, col2, col3 = st.columns([20, 45, 20])
     with col2:
         st.image("tutorial.gif")
-    # Text body
-    st.markdown("""
-
-    ### How Does It Work?
-    The web application uses web scraping techniques to gather historical data on stock mentions from Reddit, utilizing the PMAW library to access the Pushshift.io API. The scraping process is automated on a daily basis using Heroku Scheduler, making sure that the data is always up-to-date. The collected data is then stored in a PostgreSQL database hosted on Heroku for easy access and analysis.
-
-    When a user enters a stock symbol, the application employs the Pandas library to retrieve and filter relevant data from the database. This data is then utilized to generate multiple time-series graphs that display the historical popularity of the stock. These graphs allow users to identify trends in the stock's popularity over time. The application also employs the Streamlit library to create an interactive user interface, which enables users to easily search for and view stock information.
-
-    The psycopg2 and SQLAlchemy libraries are utilized to connect to the PostgreSQL database, allowing for easy querying of the data. The Pandas library is employed to filter and process the data before it is displayed in the time-series graph.
-    """)
 
 # Case 1: Display information if stock exists
 if search_input.upper() != '' and search_input.upper() in whitelist:
